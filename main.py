c4224b77764689f5eccef487f517171e9d264538
Konstantin Chaika <pro100kot14@gmail.com>
2024-07-19 17:46:24 +0400

    Add github-side running support

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from github import Github
 from github import Auth
 from collections import defaultdict
@@ -20,13 +21,9 @@ def get_from_env(var_name):
     return value
 
 
-def initLabelMapping(org):
+def initLabelMapping(org, rules):
     result = []
-    rules = get_from_env("RULES")
-    if rules is None:
-        raise Exception('Rules is not set in the environment!')
-    rules_pairs = json.loads(rules)
-    for team_name, label in rules_pairs:
+    for team_name, label in rules:
         team = org.get_team_by_slug(team_name)
         team_members = list(team.get_members())
         result.append(LabelMapping(team_name, label, team_members))
@@ -68,19 +65,24 @@ def processLabelMapping(label_mapping: LabelMapping, pull_request):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     github_event_path = get_from_env("GITHUB_EVENT_PATH")
-    with open(github_event_path, 'r') as file:
-        print(file.readlines())
-    exit(0)
+    with open(github_event_path, 'r') as github_event_file:
+        github_event = json.load(github_event_file)
+
+
     auth = Auth.Token(get_from_env("GITHUB_TOKEN"))
 
     with Github(auth=auth) as gh:
-        github_org = gh.get_organization(get_from_env("GITHUB_ORG"))
-        labelMapping = initLabelMapping(github_org)
+        github_org = gh.get_organization(github_event['organization']['login'])
+        repo = github_org.get_repo(github_event['repository']['name'])
+
+        # Getting the members of all the specified groups
+        label_rules = json.loads(get_from_env("RULES"))
+        labelMapping = initLabelMapping(github_org, label_rules)
+        # logging.info(f"labelMapping: {labelMapping}")
         print(labelMapping)
 
-        repo = github_org.get_repo(get_from_env("GITHUB_REPO"))
-
-        for pr in repo.get_pulls(state="open"):
-            for label in labelMapping:
-                processLabelMapping(label, pr)
+        pull_request = repo.get_pull(github_event['pull_request']['number'])
+        for label in labelMapping:
+            processLabelMapping(label, pull_request)
